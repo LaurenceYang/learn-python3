5ed1d058b983f7b97ddae54394541cde7e6682b9
LaurenceYang <yangy87@126.com>
2018-01-25 14:31:18 +0800

    [Add]python advance feature demo

---
+++ advance/do_generator.py
@@ -0,0 +1,60 @@
+s = (x * x for x in range(5))
+print(s)
+for x in s:
+    print(x)
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+f = fib(10)
+print('fib(10):', f)
+for x in f:
+    print(x)
+
+# call generator manually:
+g = fib(5)
+while 1:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+
+#杨辉三角
+def triangles():
+    L = [1]
+    while True:
+        yield L
+        L.append(0)
+        L = [L[i - 1] + L[i] for i in range(len(L))]
+
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t[:]) #加入拷贝
+    n = n + 1
+    if n == 10:
+        break
+print(results)
+if results == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+    [1, 6, 15, 20, 15, 6, 1],
+    [1, 7, 21, 35, 35, 21, 7, 1],
+    [1, 8, 28, 56, 70, 56, 28, 8, 1],
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+]:
+    print('测试通过!')
+else:
+    print('测试失败!')
---
+++ advance/do_listcompr.py
@@ -0,0 +1,10 @@
+print([x * x * x for x in range(1, 11)])
+print([x * x for x in range(1, 11) if x % 2 == 0])
+print([m + n for m in 'ABCD' for n in 'XYZ1'])
+
+d = {'x': 'A', 'y': 'B', 'z': 'C' }
+print([k + '=' + v for k, v in d.items()])
+
+L = ['Hello', 'World', 'IBM', 'Apple']
+print([s.upper() for s in L])
+print([s.lower() for s in L])
