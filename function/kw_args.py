673806dfa4f82ce57b8b3bbf62133d0bcc9045cc
LaurenceYang <yangy87@126.com>
2018-01-25 12:57:58 +0800

    [Update]add python function demo

---
+++ function/kw_args.py
@@ -0,0 +1,28 @@
+def print_scores(**kw):
+    print('         Name    Score')
+    print('----------------------')
+    for name, score in kw.items():
+        print('%10s %d' % (name, score))
+    print()
+
+print_scores(yang=100, liu=22, xiaoxiao=99)
+
+data = {
+    'Adam Lee': 99,
+    'Lisa S': 88,
+    'F.Bart': 77
+}
+
+print_scores(**data)
+
+def print_info(name, *, gender, city='Beijing', age):
+    print('Personal Info')
+    print('---------------')
+    print('   Name: %s' % name)
+    print(' Gender: %s' % gender)
+    print('   City: %s' % city)
+    print('    Age: %s' % age)
+    print()
+
+print_info('Bob', gender='male', age=20)
+print_info('Lisa', gender='female', city='Shanghai', age=18)
